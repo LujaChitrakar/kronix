1f89a49b9983d79dcd42cd474a916e44ed4ba5d3
spo0ds <addresszero789@proton.me>
2026-04-30 17:40:35 +0545

    fix: port bind to 8080

--- chart_server/src/main.rs
+++ chart_server/src/main.rs
@@ -303,7 +303,7 @@ async fn main() -> std::io::Result<()> {
             .service(ws::ws_handler)
     })
     // .bind(("127.0.0.1", 8081))?
-    .bind(("0.0.0.0", 8081))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
